3ddfab38b9dd1de01b52c2d9486598d565a35d81
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-12-17 11:34:04 +0530

    Board script for the game

--- ColourMatching/Assets/Scripts/Board.cs
+++ ColourMatching/Assets/Scripts/Board.cs
@@ -2,6 +2,27 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    
-  
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private TIleBackground[,] tiles;
+
+    void Start()
+    {
+        tiles = new TIleBackground[width, height];
+
+        Setup();
+    }
+
+    void Setup()
+    {
+        for(int i = 0;i<width;i++)
+        {
+            for(int j = 0;j<height;j++)
+            {
+                Vector2 tempPosition = new Vector2(i,j);
+                Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+            }
+        }
+    }
 }
